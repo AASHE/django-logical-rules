125d95403e1dedaf630e97879bfa450c4ee4396f
jamstooks <jamstooks@localhost>
2013-04-16 09:10:09 -0600

    initial commit after converting from aashe_rules;

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import os
+
+# Utility function to read README file
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='django-logical-rules',
+      version='0.1',
+      description='A rule engine for Django apps.',
+      author='Benjamin Stookey',
+      author_email='ben@aashe.org',
+      url='https://bitbucket.org/aashe/django-logical-rules',
+      long_description=read("README.md"),
+      packages=['logical_rules'],
+      install_requires=['setuptools'],
+      classifiers=[
+              'Development Status :: 4 - Beta',
+              'Environment :: Web Environment',
+              'Framework :: Django',
+              'Intended Audience :: Developers',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python',
+              'Topic :: Utilities'
+          ],
+      )
