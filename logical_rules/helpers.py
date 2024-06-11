d75e878fd61bf756af6c470d86e87ba2b929cef1
Dzemsad Dugalic <dzemsad.dugalic.fe@gmail.com>
2024-06-11 12:39:47 +0200

    fix: new syntax

--- logical_rules/helpers.py
+++ logical_rules/helpers.py
@@ -1,21 +1,22 @@
 import importlib
-from importlib import import_module
+import sys
 from django.conf import settings
 
 
 def import_rules(app):
 
     try:
-        app_path = import_module(app).__path__
+        app_path = importlib.import_module(app).__path__
     except AttributeError:
         return None
 
     try:
-        importlib.util.find_spec('rules', app_path)
-    except ImportError:
+        module_spec = importlib.util.find_spec('rules', app_path)
+        module = importlib.util.module_from_spec(module_spec)
+        module_spec.loader.exec_module(module)
+    except (ImportError, AttributeError):
         return None
 
-    module = import_module('%s.rules' % app)
     return module
 
 
